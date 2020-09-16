cc35c6418418d73fc7844dd8d7ce3e68a6c8e908
hajam09 <hajam09@yahoo.com>
2020-09-16 21:56:04 +0100

    search page update

--- tutoring/views.py
+++ tutoring/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
+from accounts.models import TutorProfile
 
 def mainpage(request):
 	if request.method == "POST":
-		search = request.POST["search"]
+		generalQuery = request.POST["generalQuery"]
 		location = request.POST["location"]
+
+		tutorList = None
+		if generalQuery and location:
+			tutorList = TutorProfile.objects.filter(summary__icontains=generalQuery, location__icontains=location) | \
+						TutorProfile.objects.filter(subjects__icontains=generalQuery, location__icontains=location)
+
+		if generalQuery:
+			tutorList = TutorProfile.objects.filter(summary__icontains=generalQuery) | \
+						TutorProfile.objects.filter(subjects__icontains=generalQuery)
+
+		if location:
+			tutorList = TutorProfile.objects.filter(location__icontains=location)
+
+		context = {"tutorList": tutorList}
+
+		if tutorList.count() == 0:
+			context["message"] = "Sorry, we couldn't find you a tutor for your search. Try entering something broad."
+			context["alert"] = "alert-info" 
+		print(context)
+		return render(request, "tutoring/mainpage.html",context)
 
 	return render(request, "tutoring/mainpage.html",{})
