aa0733f895e79ebde7987c9bd2db4d825ece7348
hajam09 <hajam09@yahoo.com>
2020-09-17 23:05:27 +0100

    question and answer

--- tutoring/models.py
+++ tutoring/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class QuestionAnswer(models.Model):
+	subject = models.CharField(max_length=1024)
+	question = models.TextField()
+	answer = models.TextField(null=True)
+	questioner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="questioner") # user who asked the question
+	answerer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="answerer") # user who is being asked the question (tutor)
+	likes = models.ManyToManyField(User, related_name='likes')
+	dislikes = models.ManyToManyField(User, related_name='dislikes')
--- tutoring/views.py
+++ tutoring/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 from accounts.models import TutorProfile
+from .models import QuestionAnswer
 
 def mainpage(request):
 	if request.method == "POST":
@@ -24,6 +26,29 @@ def mainpage(request):
 			context["message"] = "Sorry, we couldn't find you a tutor for your search. Try entering something broad."
 			context["alert"] = "alert-info" 
 		print(context)
-		return render(request, "tutoring/mainpage.html",context)
+		return render(request, "tutoring/mainpage.html", context)
 
-	return render(request, "tutoring/mainpage.html",{})
+	return render(request, "tutoring/mainpage.html", {})
+
+def viewtutorprofile(request, tutorId):
+	try:
+		tutorProfile = TutorProfile.objects.get(pk=tutorId)
+	except TutorProfile.DoesNotExist:
+		return redirect("tutoring:mainpage")
+	
+	tutorProfile.subjects = tutorProfile.subjects.split(",")
+
+	if request.method == "POST" and "postQuestion" in request.POST and request.user.is_authenticated:
+		subjectRelated = request.POST['subject-related']
+		question = request.POST['question']
+		QuestionAnswer.objects.create(subject=subjectRelated, question=question, questioner=request.user, answerer=tutorProfile.user)
+
+	questionAndAnswers = QuestionAnswer.objects.filter(answerer=tutorProfile.user)
+	for i in questionAndAnswers:
+		i.subject = i.subject.split(",")
+
+	return render(request, "tutoring/tutorprofile.html", {"tutorProfile": tutorProfile, "questionAndAnswers": questionAndAnswers})
+
+def viewstudentprofile(request, studentId):
+	print("bb")
+	return render(request, "tutoring/studentprofile.html", {})
