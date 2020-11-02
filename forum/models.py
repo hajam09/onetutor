50cf4b3167303ecb9435f5593de739bb0f5e3cb0
Mohamed Ashraf Hajamohideen <34398691+hajam09@users.noreply.github.com>
2020-11-02 20:18:59 +0000

    new updates

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
 
-# Create your models here.
+class Forum(models.Model):
+	creator = models.ForeignKey(User, on_delete=models.CASCADE)
+	forum_title = models.TextField()
+	forum_url = models.CharField(max_length=512)
+	forum_description = models.TextField()
+	created_at = models.DateTimeField(default=datetime.now)
+	anonymous = models.BooleanField()
+	forum_likes = models.ManyToManyField(User, related_name='forum_likes')
+	forum_dislikes = models.ManyToManyField(User, related_name='forum_dislikes')
+
+class SubForum(models.Model):
+	parent_forum = models.ForeignKey(Forum, on_delete=models.CASCADE)
+	creator = models.ForeignKey(User, on_delete=models.CASCADE)
+	forum_title = models.TextField()
+	forum_url = models.CharField(max_length=512)
+	forum_description = models.TextField()
+	created_at = models.DateTimeField(default=datetime.now)
+	anonymous = models.BooleanField()
+	sub_forum_likes = models.ManyToManyField(User, related_name='sub_forum_likes')
+	sub_forum_dislikes = models.ManyToManyField(User, related_name='sub_forum_dislikes')
+
+# class Comment(models.Model):
+# 	sub_forum = models.ForeignKey(SubForum, on_delete=models.CASCADE)
+# 	creator = models.ForeignKey(User, on_delete=models.CASCADE)
+# 	forum_description = models.TextField(max_length=1024)
+# 	created_at = models.DateTimeField(default=datetime.now)
+# 	anonymous = models.BooleanField()
+# 	comment_likes = models.ManyToManyField(User, related_name='comment_likes')
+# 	comment_dislikes = models.ManyToManyField(User, related_name='comment_dislikes')
--- onetutor/urls.py
+++ onetutor/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    # path('forum/', include('forum.urls')),
+    path('forum/', include('forum.urls')),
     path('', include('tutoring.urls')),
 ]
 
