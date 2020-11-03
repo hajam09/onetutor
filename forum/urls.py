33f2b5faff2cfcdb46e06a3d42b3bd1f72f2f33e
Mohamed Ashraf Hajamohideen <34398691+hajam09@users.noreply.github.com>
2020-11-03 17:13:26 +0000

    revert thread voting

--- forum/urls.py
+++ forum/urls.py
@@ -4,7 +4,5 @@ app_name = "forum"
 
 urlpatterns = [
 	path('', views.mainpage, name='mainpage'),
-	path('<slug:forum_url>/', views.forumpage, name='forumpage'),
-	# path('like_sub_forum/', views.like_sub_forum, name='like_sub_forum'),
-	# path('dislike_sub_forum/', views.dislike_sub_forum, name='dislike_sub_forum'),
+	path('viewforum/<slug:forum_url>/', views.forumpage, name='forumpage'),
 ]
