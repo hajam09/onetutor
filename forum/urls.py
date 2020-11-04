a54633ca58c01d8c393eaa5a86ce93f896710dd5
Mohamed Ashraf Hajamohideen <34398691+hajam09@users.noreply.github.com>
2020-11-04 20:07:08 +0000

    pagination fixed with errors

--- forum/urls.py
+++ forum/urls.py
@@ -4,7 +4,7 @@ app_name = "forum"
 
 urlpatterns = [
 	path('<slug:page_number>/', views.mainpage, name='mainpage'),
-	path('viewforum/<slug:forum_url>/', views.forumpage, name='forumpage'),
-	path('upvote_sub_forum/', views.upvote_sub_forum, name='upvote_sub_forum'),
-	path('downvote_sub_forum/', views.downvote_sub_forum, name='downvote_sub_forum'),
+	path('community/<slug:forum_url>/<slug:page_number>/', views.parentforumpage, name='parentforumpage'),
+	path('vote/upvote_sub_forum/', views.upvote_sub_forum, name='upvote_sub_forum'),
+	path('vote/downvote_sub_forum/', views.downvote_sub_forum, name='downvote_sub_forum'),
 ]
