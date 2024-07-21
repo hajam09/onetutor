8e47ac334fcb6d90918886ef54d80da003abee9f
Mohamed Ashraf Hajamohideen <hajam09@yahoo.com>
2024-07-21 23:02:17 +0100

    model update

--- core/models.py
+++ core/models.py
@@ -1,3 +1,151 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+from accounts.models import BaseModel
+
+
+class Availability(BaseModel):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True, related_name='availability')
+    sundayMorning = models.BooleanField(default=False)
+    sundayAfternoon = models.BooleanField(default=False)
+    sundayEvening = models.BooleanField(default=False)
+    mondayMorning = models.BooleanField(default=False)
+    mondayAfternoon = models.BooleanField(default=False)
+    mondayEvening = models.BooleanField(default=False)
+    tuesdayMorning = models.BooleanField(default=False)
+    tuesdayAfternoon = models.BooleanField(default=False)
+    tuesdayEvening = models.BooleanField(default=False)
+    wednesdayMorning = models.BooleanField(default=False)
+    wednesdayAfternoon = models.BooleanField(default=False)
+    wednesdayEvening = models.BooleanField(default=False)
+    thursdayMorning = models.BooleanField(default=False)
+    thursdayAfternoon = models.BooleanField(default=False)
+    thursdayEvening = models.BooleanField(default=False)
+    fridayMorning = models.BooleanField(default=False)
+    fridayAfternoon = models.BooleanField(default=False)
+    fridayEvening = models.BooleanField(default=False)
+    saturdayMorning = models.BooleanField(default=False)
+    saturdayAfternoon = models.BooleanField(default=False)
+    saturdayEvening = models.BooleanField(default=False)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['user'], name='availability-idx-user'),
+        ]
+        verbose_name_plural = 'Availability'
+
+
+class Question(BaseModel):
+    topic = models.CharField(max_length=1024)
+    query = models.CharField(max_length=2048)
+    asker = models.ForeignKey(User, on_delete=models.CASCADE, related_name='askerQuestions')
+    tutor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tutorQuestions')
+    likes = models.ManyToManyField(User, related_name='questionLikes')
+    dislikes = models.ManyToManyField(User, related_name='questionDislikes')
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['asker'], name='question-idx-asker'),
+            models.Index(fields=['tutor'], name='question-idx-tutor'),
+        ]
+        verbose_name_plural = 'TutorQuestion'
+
+    def like(self, request):
+        if request.user not in self.likes.all():
+            self.likes.add(request.user)
+        else:
+            self.likes.remove(request.user)
+
+        if request.user in self.dislikes.all():
+            self.dislikes.remove(request.user)
+
+    def dislike(self, request):
+        if request.user not in self.dislikes.all():
+            self.dislikes.add(request.user)
+        else:
+            self.dislikes.remove(request.user)
+
+        if request.user in self.likes.all():
+            self.likes.remove(request.user)
+
+
+class Response(BaseModel):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='questionResponses')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='authorResponses')
+    content = models.TextField()
+    likes = models.ManyToManyField(User, related_name='responseLikes')
+    dislikes = models.ManyToManyField(User, related_name='responseDislikes')
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['question'], name='response-idx-question'),
+            models.Index(fields=['user'], name='response-idx-user'),
+        ]
+        verbose_name_plural = 'Response'
+
+    def like(self, request):
+        if request.user not in self.likes.all():
+            self.likes.add(request.user)
+        else:
+            self.likes.remove(request.user)
+
+        if request.user in self.dislikes.all():
+            self.dislikes.remove(request.user)
+
+    def dislike(self, request):
+        if request.user not in self.dislikes.all():
+            self.dislikes.add(request.user)
+        else:
+            self.dislikes.remove(request.user)
+
+        if request.user in self.likes.all():
+            self.likes.remove(request.user)
+
+
+class TutorReview(BaseModel):
+    tutor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tutorReviews')
+    reviewer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviewer')
+    comment = models.TextField()
+    rating = models.PositiveSmallIntegerField()
+    likes = models.ManyToManyField(User, related_name='tutorReviewLikes')
+    dislikes = models.ManyToManyField(User, related_name='tutorReviewDislikes')
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['tutor'], name='tutor-review-idx-tutor'),
+            models.Index(fields=['reviewer'], name='tutor-review-idx-reviewer'),
+        ]
+        verbose_name_plural = 'TutorReview'
+
+    def like(self, request):
+        if request.user not in self.likes.all():
+            self.likes.add(request.user)
+        else:
+            self.likes.remove(request.user)
+
+        if request.user in self.dislikes.all():
+            self.dislikes.remove(request.user)
+
+    def dislike(self, request):
+        if request.user not in self.dislikes.all():
+            self.dislikes.add(request.user)
+        else:
+            self.dislikes.remove(request.user)
+
+        if request.user in self.likes.all():
+            self.likes.remove(request.user)
+
+
+class Lesson(BaseModel):
+    tutor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tutorLessons')
+    student = models.ForeignKey(User, on_delete=models.CASCADE, related_name='studentLessons')
+    hoursTaught = models.DecimalField(max_digits=4, decimal_places=2)
+    points = models.PositiveSmallIntegerField()
+    amount = models.DecimalField(max_digits=5, decimal_places=2)  # amount = hoursTaught * tutor.chargeRate
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['tutor'], name='lesson-idx-tutor'),
+            models.Index(fields=['student'], name='lesson-idx-student'),
+        ]
+        verbose_name_plural = 'Lesson'
--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,91 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+from accounts.models import BaseModel, generateRandomString
+
+
+class Category(BaseModel):
+    name = models.CharField(max_length=1024, unique=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['name'], name='category-idx-name'),
+        ]
+        verbose_name_plural = 'Category'
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+
+class Forum(BaseModel):
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=1024)
+    url = models.CharField(max_length=8, default=generateRandomString)
+    description = models.TextField(blank=True, null=True)
+    anonymous = models.BooleanField(default=False)
+    likes = models.ManyToManyField(User, related_name='forumLikes')
+    dislikes = models.ManyToManyField(User, related_name='forumDislikes')
+    picture = models.ImageField(blank=True, null=True, upload_to='forum-image/')
+    watchers = models.ManyToManyField(User, related_name='forumWatchers')
+
+    def like(self, request):
+        if request.user not in self.likes.all():
+            self.likes.add(request.user)
+        else:
+            self.likes.remove(request.user)
+
+        if request.user in self.dislikes.all():
+            self.dislikes.remove(request.user)
+
+    def dislike(self, request):
+        if request.user not in self.dislikes.all():
+            self.dislikes.add(request.user)
+        else:
+            self.dislikes.remove(request.user)
+
+        if request.user in self.likes.all():
+            self.likes.remove(request.user)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['creator'], name='forum-idx-creator'),
+            models.Index(fields=['title'], name='forum-idx-title'),
+            models.Index(fields=['description'], name='forum-idx-description'),
+        ]
+        verbose_name_plural = 'Forum'
+
+
+class ForumComment(BaseModel):
+    forum = models.ForeignKey(Forum, on_delete=models.CASCADE, related_name='forumComments')
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.TextField()
+    anonymous = models.BooleanField(default=False)
+    likes = models.ManyToManyField(User, related_name='forumCommentLikes')
+    dislikes = models.ManyToManyField(User, related_name='forumCommentDislikes')
+
+    def like(self, request):
+        if request.user not in self.likes.all():
+            self.likes.add(request.user)
+        else:
+            self.likes.remove(request.user)
+
+        if request.user in self.dislikes.all():
+            self.dislikes.remove(request.user)
+
+    def dislike(self, request):
+        if request.user not in self.dislikes.all():
+            self.dislikes.add(request.user)
+        else:
+            self.dislikes.remove(request.user)
+
+        if request.user in self.likes.all():
+            self.likes.remove(request.user)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['forum'], name='forum-comment-idx-forum'),
+            models.Index(fields=['creator'], name='forum-comment-idx-creator'),
+            models.Index(fields=['forum', 'creator'], name='forum-comment-idx-book-creator'),
+        ]
+        verbose_name_plural = 'ForumComment'
